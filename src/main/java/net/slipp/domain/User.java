2918fb677362ff2a3b6fe9ff108a17b0ca1a2549
Jaewon Choi <qwebnm7788@naver.com>
2018-06-27 20:57:21 +0900

    jpa 설정 추가, url 리팩토링, html 중복 제거

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -1,10 +1,26 @@
-package net.slipp.web;
+package net.slipp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable=false, length=20)
 	private String userId;
+	
 	private String password;
 	private String name;
 	private String email;
+	
+	public Long getId() {
+		return id;
+	}
 
 	public String getUserId() {
 		return userId;
@@ -38,9 +54,16 @@ public class User {
 		this.email = email;
 	}
 
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+	}
+	
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
+
 
 }
