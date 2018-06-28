1416b5d4b1d87660d6de1a6a9bf052a27729d659
Jaewon Choi <qwebnm7788@naver.com>
2018-06-28 10:11:56 +0900

    로그인 기능 구현

--- src/main/java/net/slipp/domain/UserRepository.java
+++ src/main/java/net/slipp/domain/UserRepository.java
@@ -2,6 +2,6 @@ package net.slipp.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
+public interface UserRepository extends JpaRepository<User, Long> {
+	User findByUserId(String userId);
 }
